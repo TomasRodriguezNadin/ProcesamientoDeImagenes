13a6c1907f29b31d0d623bbb6ec06e531a2a88a4
Tomas Rodriguez Nadin <tnadin@dc.uba.ar>
2025-09-24 13:24:28 -0300

    Sacamos la niebla pasando los grises a negro

--- sacarRuido.py
+++ sacarRuido.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, util
-from skimage.color import rgb2hsv, hsv2rgb
-from skimage.filters import gaussian
+from skimage.color import rgb2hsv, hsv2rgb, rgb2gray
+from skimage.feature import canny
 
 
 def pltImagenConH(imagen, imagenH, nombre, nombreH, axs, columna):
@@ -29,32 +29,13 @@ def pltImagenConH(imagen, imagenH, nombre, nombreH, axs, columna):
     imagenMascara = hsv2rgb(imagenMascara)
     axs[2][columna].set_title("mascara no negros")
     axs[2][columna].axis("off")
-    axs[2][columna].imshow(imagenMascara, clim=(0, 1))
-
-
-def esBlanco(pixel):
-    return pixel[1] == 0 and pixel[2] == 1
-
-
-def esNegro(pixel):
-    return pixel[1] == 0 and pixel[2] == 0
+    axs[2][columna].imshow(canny(rgb2gray(imagen)), clim=(0, 1))
 
 
 def promedioSaltAndPepper(imagen):
-    copia = rgb2hsv(imagen)
-
-    for i in range(copia.shape[0]):
-        for j in range(copia.shape[1]):
-            if esBlanco(copia[i, j]):
-                copia[i, j] = 0
-
+    copia = sacarGrises(imagen)
     copia = promedioAritmetico(copia)
     return hsv2rgb(copia)
-
-
-def unsharpMasking(imagen):
-    gausiana = gaussian(imagen)
-    return np.clip(imagen - gausiana, 0, 1)
 
 
 # Promedio geometrico de Digital Image Processing (Gonzales, Woods)
@@ -81,6 +62,13 @@ def promedioAritmetico(imagen):
     return res
 
 
+def sacarGrises(imagen):
+    imagenHSV = rgb2hsv(imagen)
+    canalSaturacion = imagenHSV[:, :, 1]
+    imagenHSV[canalSaturacion == 0] = 0
+    return hsv2rgb(imagenHSV)
+
+
 imagenNormal = util.img_as_float64(io.imread("./shape_dataset/image_0000.png"))
 imagenRuidosa = util.img_as_float64(io.imread("./shape_dataset/image_0003.png"))
 imagenNiebla = util.img_as_float64(io.imread("./shape_dataset/image_0008.png"))
@@ -89,8 +77,8 @@ imagenSaltAndPepper = util.img_as_float64(io.imread("./shape_dataset/image_0012.
 fig, axs = plt.subplots(3, 4, figsize=(20, 10))
 pltImagenConH(imagenNormal, promedioSaltAndPepper(imagenNormal), "Imagen Normal", "Promedio aritmetico", axs, 0)
 pltImagenConH(imagenRuidosa, promedioGeometrico(imagenRuidosa), "Imagen Ruidosa", "Promedio Geometrico", axs, 1)
-pltImagenConH(imagenNiebla, promedioGeometrico(imagenNiebla), "Imagen Niebla", "Promedio Geometrico", axs, 2)
-pltImagenConH(imagenSaltAndPepper, promedioSaltAndPepper(imagenSaltAndPepper), "Imagen SaltAndPepper", "Suprimir blancos y promediar por vecinos", axs, 3)
+pltImagenConH(imagenNiebla, sacarGrises(imagenNiebla), "Imagen Niebla", "sacar grises", axs, 2)
+pltImagenConH(imagenSaltAndPepper, sacarGrises(imagenSaltAndPepper), "Imagen SaltAndPepper", "sacarGrises", axs, 3)
 
 plt.tight_layout()
 plt.show()
